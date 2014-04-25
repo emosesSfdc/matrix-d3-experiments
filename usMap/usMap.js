8c8e881a76c29fbb124e5e71de3aa1754935cf3e
Evan Moses <emoses@salesforce.com>
2014-04-25 14:21:46 -0700

    Pretty colors for states, overlay

--- usMap/usMap.js
+++ usMap/usMap.js
@@ -14,11 +14,18 @@ var svg = d3.select("body").append("svg")
 
 colorScale = d3.scale.linear()
     .domain([0, 100])
-    .range([0, 255]);
+    .range(["blue", "red"]);
 
 var states =["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
 
 var stateCount = states.reduce(function(m, val){m[val] = 0; return m;}, {});
+
+function easeBigBounce(t){
+    if (t <= 0) return 0;
+    if (t >= 1) return 1;
+    var quarterTpi = (t-0.25)*Math.PI;
+    return 3 * (Math.sin(4*quarterTpi)/quarterTpi);
+}
 
 function nextState(){
     var index = Math.floor(Math.random() * states.length);
@@ -31,16 +38,45 @@ function startRandom(){
 	var count = stateCount[s];
 	count += 1;
 	stateCount[s] = count;
-	document.getElementById("state-" + s).style.fill = d3.rgb(colorScale(count), 0, 0).toString();
+	d3.select("#state-" + s)
+	   .transition()
+	   .duration(200)
+	   .ease(easeBigBounce)
+	   .style("fill", colorScale(count));
     }, 50);
 }
 
-d3.json("us-states-named.json", function(error, us){
+function updateInfo(d){
+    infoDiv = d3.select("#info");
+    infoDiv.html("<h3>" + d.properties.name + '</h3><div class="dataBlock"><span class="name">Count:</span><span class="data">' + stateCount[d.properties.code] + '</span></div>')
+}
+
+function mouseMove(){
+    d3.select('#infoCtr')
+	.transition()
+	.duration(100)
+	.style("left", (d3.event.pageX) + "px")
+	.style("top", (d3.event.pageY + 50) + "px")
+    var state = d3.event.toElement && d3.select(d3.event.toElement).classed("state") ? d3.event.toElement : null;
+    var info = d3.select("#info");
+    if (state){
+	var d3State = d3.select(state);
+	updateInfo(d3State.datum())
+	info.transition()
+	    .duration(300)
+	    .style("opacity", 0.9);
+    } else {
+	info.transition()
+	    .duration(300)
+	    .style("opacity", 0);
+    }
+
+}
+
+function loadStates(error, us){
     var states = topojson.feature(us, us.objects.states);
-    svg.insert("path", ".graticule")
-       .datum(states)
-       .attr("class", "land")
-       .attr("d", path);
+
+    svg.on("mousemove", mouseMove);
 
     svg.selectAll("path")
      .data(states.features)
@@ -54,8 +90,11 @@ d3.json("us-states-named.json", function(error, us){
        .attr("class", "state-boundry")
        .attr("d", path);
 
-    startRandom();
-});
+};
+
+queue()
+   .defer(d3.json, "us-states-named.json")
+   .await(function(){loadStates.apply(null, arguments); startRandom();});
 
 
 //d3.select(self.frameElement).style("height", height + "px");
