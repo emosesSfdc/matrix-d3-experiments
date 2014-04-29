75b70d35a00477d4ef04e1a46b50faefc57d921d
Evan Moses <emoses@salesforce.com>
2014-04-29 14:57:41 -0700

    Added in text for region/count

--- usMap/zip3Map.js
+++ usMap/zip3Map.js
@@ -17,7 +17,7 @@ var g = svg.append("g");
 var zoom = d3.behavior.zoom()
     .translate([0,0])
     .scale(1)
-    .scaleExtent([1,8])
+    .scaleExtent([1,16])
     .on("zoom", zoomed);
 
 svg.call(zoom).call(zoom.event);
@@ -27,7 +27,7 @@ var colorScale = d3.scale.linear()
     .range(["blue", "red"]);
 
 
-var allZips;
+var allRegions;
 function easeBigBounce(t){
     if (t <= 0) return 0;
     if (t >= 1) return 1;
@@ -35,24 +35,32 @@ function easeBigBounce(t){
     return 5 * (Math.sin(4*quarterTpi)/quarterTpi);
 }
 
-function nextZip(){
-    var index = Math.floor(Math.random() * allZips.length);
-    return allZips[index];
+function nextRegion(){
+    var index = Math.floor(Math.random() * allRegions.length);
+    return allRegions[index];
 }
 
 function startRandom(){
     setInterval(function(){
-	var s = nextZip();
-	var zip = d3.select("#zip-" + s);
-	var datum = zip.datum();
-	datum.properties.count += 1;
+	var s = nextRegion();
+	var region = d3.select("#region-" + s);
 
-	zip.datum(datum)
-	   .transition()
-	   .duration(800)
-	   .ease(easeBigBounce)
-	   .style("fill", function(d){return colorScale(d.properties.count);});
+	var datum = region.datum();
+	datum.properties.count += 1;
+	region.datum(datum);
+
+	updateRegion(region, "count");
     }, 50);
+}
+
+function updateRegion(region, property){
+    region.select("path")
+       .transition()
+       .duration(800)
+       .ease(easeBigBounce)
+       .style("fill", function(d){return colorScale(d.properties[property]);});
+
+    region.select("text").text(regionText);
 }
 
 function updateInfo(d){
@@ -60,17 +68,28 @@ function updateInfo(d){
     infoDiv.html("<h3>" + d.properties.zip3 + '</h3><div class="dataBlock"><span class="name">Count:</span><span class="data">' + d.properties.count + '</span></div>')
 }
 
+function findParentWithClass(node, className){
+   var parent = node;
+   while (parent && parent != document.body){
+       if (d3.select(parent).classed(className)){
+	   return parent;
+       }
+       parent = parent.parentNode;
+   }
+   return false; 
+};
+
 function mouseMove(){
     d3.select('#infoCtr')
 	.transition()
 	.duration(100)
 	.style("left", (d3.event.pageX) + "px")
 	.style("top", (d3.event.pageY + 50) + "px")
-    var zip = d3.event.toElement && d3.select(d3.event.toElement).classed("zip") ? d3.event.toElement : null;
+    var region = findParentWithClass(d3.event.toElement, "regionG");
     var info = d3.select("#info");
-    if (zip){
-	var d3Zip = d3.select(zip);
-	updateInfo(d3Zip.datum())
+    if (region){
+	var d3Region = d3.select(region);
+	updateInfo(d3Region.datum())
 	info.transition()
 	    .duration(300)
 	    .style("opacity", 0.9);
@@ -82,30 +101,46 @@ function mouseMove(){
 
 }
 
+function regionText(d){
+    return d.properties.zip3 + ": " + d.properties.count; 
+}
 
-function loadZips(error, us){
-    var zips = topojson.feature(us, us.objects.zip3);
-    //Add in count to each zip
-    zips.features.forEach(function(d) {d.properties.count = 0;});
-    allZips = zips.features.map(function(d){return d.properties.zip3;});
+
+function loadRegions(error, us){
+    var regions = topojson.feature(us, us.objects.zip3);
+    //Add in count to each region
+    regions.features.forEach(function(d) {d.properties.count = 0;});
+    allRegions = regions.features.map(function(d){return d.properties.zip3;});
     /*
-    zipCount = allZips.reduce(function(m, z){m[z] = 0; return m;}, {});
+    regionCount = allRegions.reduce(function(m, z){m[z] = 0; return m;}, {});
     */
 
     g.on("mousemove", mouseMove);
 
-    g.selectAll("path")
-     .data(zips.features)
-     .enter().append("path")
-       .attr("class", "zip")
-       .attr("d", path)
-       .attr("id", function(d) {return "zip-" + d.properties.zip3;});
+    var regions = g.selectAll("path")
+     .data(regions.features)
+     .enter().append("g")
+       .attr("class", "regionG")
+       .attr("id", function(d) {return "region-" + d.properties.zip3;});
+
+    regions.append("path")
+       .attr("class", "region")
+       .attr("d", path);
+
+    regions.append("text")
+       .text(regionText)
+       .attr("x", function(d){ return path.centroid(d)[0];})
+       .attr("y", function(d){ return path.centroid(d)[1];})
+       .style("font-size", function(d) {var bounds = path.bounds(d);
+					var width = bounds[1][0] - bounds[0][0];
+					return (width / 6.0) + "px" })
+       .attr("text-anchor", "middle")
+       .attr("dy", ".35em");
 
     g.insert("path", ".graticule")
        .datum(topojson.mesh(us, us.objects.zip3, function(a, b) {return a !== b; }))
-       .attr("class", "zip-boundry")
+       .attr("class", "region-boundry")
        .attr("d", path);
-
 };
 
 function zoomed(){
@@ -127,7 +162,7 @@ function loadDone(){
 
 queue()
    .defer(d3.json, "zip3-simp.json")
-   .await(function(){loadZips.apply(null, arguments); loadDone()});
+   .await(function(){loadRegions.apply(null, arguments); loadDone()});
 
 var socket = io.connect("http://devstack.sfdc-matrix.net:3020/viewOutput");
 socket.on('topicMessage', function(data){
