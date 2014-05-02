a3c9c193026cb0d0c748827038af82c8720bbbdc
Evan Moses <emoses@salesforce.com>
2014-05-02 15:54:42 -0700

    Adding nv.d3.js histogram example

---
+++ usMap/rtHistogram.js
@@ -0,0 +1,44 @@
+var width = 900;
+var height = 700;
+
+var histData = [{key: "zipcodes", values: d3.range(0, 99).map(function(z) { return {"zip2": z.toString(), "count": 0};})}];
+
+var chart;
+nv.addGraph(function(){
+    chart = nv.models.discreteBarChart()
+	    .x(function(d) {return d.zip2;})
+            .y(function(d) {return d.count;})
+            .tooltips(true)
+            .showValues(true)
+            .transitionDuration(100);
+
+    var svg = d3.select("#chart").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.datum(histData)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+function randomData() {
+    return Math.floor(Math.random() * 100).toString();
+}
+
+function startRandom() {
+    setInterval(function(){
+	var s = randomData();
+	histData[0].values[s].count += 1; 
+	chart.update();
+    }, 100);
+}
+
+function onLoad(){
+    d3.select("#randomStart")
+	.on("click", startRandom);
+}
+
+onLoad();
